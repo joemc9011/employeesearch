1303e69c5b37e98c7bd9e75d65439f7af2bf0657
joemc9011 <joemc9011@gmail.com>
2021-01-22 20:08:05 -0500

    more backend and npms

---
+++ employee/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import compontents here
+
+function App() {
+
+}
+ 
+export default App;
--- employee/Index.js
+++ employee/Index.js
@@ -2,4 +2,5 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./App.js"
 
+// main stream of entire app being sent to root
 ReactDom.render(<App />, document.getElementById("root"));
